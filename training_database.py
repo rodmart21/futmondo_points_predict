a8fb09b7316755b697c0cca333b86a3b07de3e7c
rodmart21 <romaralo2114@gmail.com>
2025-12-14 19:22:35 +0100

    test push

--- training_database.py
+++ training_database.py
@@ -2,6 +2,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
 # Load environment variables from .env file
 load_dotenv()
